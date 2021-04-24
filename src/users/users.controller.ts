bf68758296a13d29b0185a148623fe94f1ae50d1
emersonu <emerson.arj@gmail.com>
2021-04-24 15:27:20 -0300

    endpoint POST create user

---
+++ src/users/users.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { ReturnUserDto } from './dto/return-user.dto';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private userService: UsersService) { }
+
+  @Post()
+  async createUser(@Body() createUserDto: CreateUserDto): Promise<ReturnUserDto> {
+    const user = await this.userService.createUser(createUserDto);
+    return {
+      user: user,
+      message: 'Usuário criado com sucesso!'
+    }
+  }
+}
