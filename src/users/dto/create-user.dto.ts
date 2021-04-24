380237ccceba38e343bc4d2c6aef92883cadfb77
emersonu <emerson.arj@gmail.com>
2021-04-24 15:49:33 -0300

    adicionado validaçoes ao DTO usando o pacote class-validator

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,41 @@
+import { IsEmail, IsNotEmpty, MaxLength, MinLength } from "class-validator";
+
 export class CreateUserDto {
+  @IsNotEmpty({
+    message: 'Email é obrigatório'
+  })
+  @IsEmail(
+    {},
+    {
+      message: 'Endereço de email inválido'
+    }
+  )
+  @MaxLength(200, {
+    message: 'O email não pode ter mais que 200 caracteres.'
+  })
   email: string;
+
+  @IsNotEmpty({
+    message: 'Nome é obrigatório'
+  })
+  @MaxLength(200, {
+    message: 'O nome não pode ter mais que 200 caracteres.'
+  })
   name: string;
+
+  @IsNotEmpty({
+    message: 'A senha é obrigatória',
+  })
+  @MinLength(6, {
+    message: 'A senha deve ter pelo menos 6 caracteres',
+  })
   password: string;
+
+  @IsNotEmpty({
+    message: 'A confirmação de senha é obrigatória',
+  })
+  @MinLength(6, {
+    message: 'A confirmação de senha deve ter pelo menos 6 caracteres',
+  })
   passwordConfirmation: string;
 }
