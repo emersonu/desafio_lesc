9746c699a0e2dbd7243fa769029a852eedc085c8
emersonu <emerson.arj@gmail.com>
2021-04-24 15:12:49 -0300

    chamando metodo createUser no service de Users utilizando o DTO

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './user.entity';
 import { UserRepository } from './users.repository';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(UserRepository)
-    private UserRepository: UserRepository,
+    private userRepository: UserRepository,
   ) { }
+
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
+    if (createUserDto.password != createUserDto.passwordConfirmation) {
+      throw new UnprocessableEntityException('As senhas não são iguais!');
+    } else {
+      return this.userRepository.createUser(createUserDto);
+    }
+  }
 }
