5a18750eb93631928a1fbece1c281b9612c7d59a
emersonu <emerson.arj@gmail.com>
2021-04-27 23:50:16 -0300

    testando authentication

--- src/authentication/authentication.controller.ts
+++ src/authentication/authentication.controller.ts
@@ -1,6 +1,9 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards, ValidationPipe } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { CredentialsDto } from 'src/users/dto/credentials.dto';
+import { User } from 'src/users/user.entity';
 import { AuthenticationService } from './authentication.service';
+import { GetUser } from './get-user.decorator';
 
 @Controller('authentication')
 export class AuthenticationController {
@@ -9,5 +12,11 @@ export class AuthenticationController {
   @Post('/login')
   async login(@Body(ValidationPipe) credentialsDto: CredentialsDto): Promise<{ token: string }> {
     return { token: await this.authenticationService.login(credentialsDto) };
+  }
+
+  @Get('/me')
+  @UseGuards(AuthGuard())//restringe acesso a usuários autenticados
+  getMe(@GetUser() user: User): User {
+    return user;
   }
 }
--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -4,17 +4,21 @@ import { AuthenticationController } from './authentication.controller';
 import { AuthenticationService } from './authentication.service';
 import { UserRepository } from '../users/users.repository'
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([UserRepository]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
       secret: 'super-secret',
       signOptions: {
-        expiresIn: 1800
+        expiresIn: 18000
       }
     })],
   controllers: [AuthenticationController],
-  providers: [AuthenticationService]
+  providers: [AuthenticationService, JwtStrategy],
+  exports: [JwtStrategy, PassportModule]
 })
 export class AuthenticationModule { }
